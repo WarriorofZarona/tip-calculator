554676bd07a94c35a421142a039c998c5c617d19
Andrew Bautista <andrewbautista85@gmail.com>
2020-01-11 12:11:47 -0500

    initial commit, already made in system and wanting to use it online

---
+++ script.js
@@ -0,0 +1,37 @@
+var totalPriceEl = document.querySelector("#total-price");
+var tipEl = document.querySelector("#tip-percentage");
+var submitEl = document.querySelector("#submit");
+var tipAmountEl = document.querySelector("#tip-amount");
+var newTotalEl = document.querySelector("#new-total")
+
+
+function convertTip() {
+
+
+    var tipVal = (parseFloat(tipEl.value) * .01).toFixed(2);
+
+    console.log(tipVal);
+    return tipVal
+
+}
+
+submitEl.addEventListener("click", function (event) {
+
+    event.preventDefault();
+
+    var totalPrice = totalPriceEl.value;
+
+    var tipTotal = (totalPrice * convertTip()).toFixed(2);
+
+    console.log(tipTotal)
+    tipAmountEl.textContent = tipTotal;
+
+
+    var newPrice = (parseFloat(tipTotal) + parseFloat(totalPrice)).toFixed(2);
+
+    console.log(newPrice)
+
+    newTotalEl.textContent = newPrice
+
+
+});
